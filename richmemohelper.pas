e41e611be38a06c89cee661b57d67e92b2e52822
Alexander Tverskoy <astverskoy@gmail.com>
2026-08-19 21:32:44 +0300

    Added module headers

--- richmemohelper.pas
+++ richmemohelper.pas
@@ -1,3 +1,9 @@
+//-----------------------------------------------------------------------------------
+//  RichKit Package © 2026 by Alexander Tverskoy
+//  Licensed under the MIT License
+//  You may obtain a copy of the License at https://opensource.org/licenses/MIT
+//-----------------------------------------------------------------------------------
+
 unit RichMemoHelper;
 
 {$mode objfpc}{$H+}
